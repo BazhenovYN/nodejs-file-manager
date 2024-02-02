00e5d52712068c653b7a40c287f33c008cb3eec6
Iurii Bazhenov <bazhenovyn@gmail.com>
2024-02-02 21:02:39 +0100

    fix: fix typo

--- src/errors.js
+++ src/errors.js
@@ -1,6 +1,6 @@
 export const errors = {
   failed: "Operation failed",
   unknownCommand: "Invalid input: Unknown command",
-  unknownParams: "Invalid input: Unknown parametr",
+  unknownParams: "Invalid input: Unknown parameter",
   noParams: "Invalid input: Command parameters not set",
 };
